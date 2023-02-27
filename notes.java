83d6f20edd5789d606a880df8ea1d170795bf524
Zichen Zhang <zhangzzc@umich.edu>
2023-02-27 15:19:55 -0500

    revision

---
+++ notes.java
@@ -0,0 +1,6 @@
+/* Grade Scripts is an open source app building tool */
+
+/* The Manifests folder has a xml file which contains information about the app */
+
+/* Views are defined as all kinds of UI elements */
+
