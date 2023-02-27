50584b42a0392908980d6ecd21e70405ce315a1f
Zichen Zhang <zhangzzc@umich.edu>
2023-02-27 15:49:55 -0500

    revise

--- notes.java
+++ notes.java
@@ -4,3 +4,7 @@
 
 /* Views are defined as all kinds of UI elements */
 
+/* text size: 
+    sp is "scale dependent pixel". It will be affected if the user wants all the text to be
+    bigger or smaller. But dp will not be affected by this. So, we want to use dp for design
+    and sp for text size.
